fb53a01778167647df9cf125c5aabe101ed49e61
Robin Kluth <kontakt@kluthr.de>
2024-06-05 14:22:58 +0200

    Initial

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This helper updates your netcup domain with current IPv4/6 IPs. It has support f
 * `NC_UPDATE_IP6_PREFIX` true, if only prefix should be updated, false otherwise
 * `NC_DOMAIN` domain (zone) name to use
 * `NC_SUBDOMAINS` comma seperated list of entries to update. Example: `intern` or `intern,cloud`
+
+## How to run?
+Simply start the container and call `/run.php`. Example: `http://1.2.3.4/run.php`.
--- src/run.php
+++ src/run.php
@@ -1,19 +1,33 @@
 <?php
 
-$version         = '1.0.0';
+$LOG = [];
+
+function _log($msg) {
+    global $LOG;
+    $_LOG[] = $msg;
+    echo $msg.'<br />';
+}
+
+$version         = '1.0.2';
 $sessionId       = null;
 $toUpdateRecords = [];
 $identSvcs       = ['v4.ident.me', 'v6.ident.me', 'v4.tnedi.me', 'v6.tnedi.me'];
+
+_log("Welcome to version $version of this tool!");
+_log("Configured Ident services: ".implode(", ", $identSvcs));
 
 $myIP4 = null;
 $myIP6 = null;
 
 if (empty(getenv("NC_SUBDOMAINS")) || empty(getenv("NC_DOMAIN"))) {
-    echo "No domains to handle!";
+    _log("No domains to handle!");
     exit;
 }
 
 $domainlist = explode(",", getenv("NC_SUBDOMAINS"));
+
+_log("Configured zone: ".getenv("NC_DOMAIN"));
+_log("Configured entries to update: ".implode(", ", $domainlist));
 
 
 foreach ($identSvcs as $identSvc) {
@@ -22,7 +36,7 @@ foreach ($identSvcs as $identSvc) {
     $res = curl_exec($ch);
 
     if (!$res) {
-        echo "Error contacting $identSvc";
+        _log("Error contacting $identSvc");
         continue;
     }
 
@@ -34,7 +48,7 @@ foreach ($identSvcs as $identSvc) {
             $prefix_new = explode(":", $myIP6, 5);
         }
     } else {
-        echo "Invalid IP: $res" . PHP_EOL;
+        _log("Invalid IP: $res");
     }
 
     if (!empty($myIP4) && !empty($myIP6)) {
@@ -44,12 +58,12 @@ foreach ($identSvcs as $identSvc) {
 }
 
 if (empty($myIP4) && empty($myIP6)) {
-    echo "Cannot get any new IP!";
+    _log("Cannot get any new IP!");
     exit;
 }
 
-echo "New IP4: $myIP4";
-echo "New IP6: $myIP6";
+_log("New IP4: $myIP4");
+_log("New IP6: $myIP6");
 
 
 require_once('DomainWebserviceSoapClient.php');
@@ -59,8 +73,8 @@ $nc = new DomainWebserviceSoapClient();
 $r = $nc->login(getenv('NC_KD'), getenv('NC_APIKEY'), getenv('NC_APIPW'), 'NC DDNS Updater v' . $version);
 
 if (!$r || $r->status != 'success') {
-    echo "Login failed!";
-    print_r($r);
+    _log("Login failed!");
+    _log(print_r($r, true));
     exit;
 }
 
@@ -69,8 +83,8 @@ $sessionId = $r->responsedata->apisessionid;
 
 $r = $nc->infoDnsRecords(getenv("NC_DOMAIN"), getenv('NC_KD'), getenv('NC_APIKEY'), $sessionId, 'NC DDNS Updater v' . $version);
 if (!$r || $r->status != 'success') {
-    echo "Get domains failed!";
-    print_r($r);
+    _log("Get domains failed!");
+    _log(print_r($r, true));
     exit;
 }
 
@@ -80,10 +94,10 @@ $dnsRecordSet->dnsrecords = $r->responsedata->dnsrecords;
 foreach ($dnsRecordSet->dnsrecords as $dnsrecord) {
     if (in_array($dnsrecord->hostname, $domainlist)) {
         if (!in_array($dnsrecord->type, ['A', 'AAAA'])) {
-            echo "$dnsrecord->hostname is not A/AAAA. Its " . $dnsrecord->type;
+            _log("$dnsrecord->hostname is not A/AAAA. Its " . $dnsrecord->type);
             continue;
         }
-        echo "Updating $dnsrecord->type $dnsrecord->hostname";
+        _log("Processing $dnsrecord->type: $dnsrecord->hostname");
 
         $newIP = null;
 
@@ -93,6 +107,7 @@ foreach ($dnsRecordSet->dnsrecords as $dnsrecord) {
                 break;
             case 'AAAA':
                 if (getenv("NC_UPDATE_IP6_PREFIX") == 'true') {
+                    _log("Only updating v6 prefix!");
                     $prefix_old    = explode(":", $dnsrecord->destination, 5);
                     $prefix_old[0] = $prefix_new[0];
                     $prefix_old[1] = $prefix_new[1];
@@ -104,16 +119,16 @@ foreach ($dnsRecordSet->dnsrecords as $dnsrecord) {
                 break;
         }
         if ($dnsrecord->destination == $newIP || empty($newIP)) {
-            echo "No need to update - same IP!";
+            _log("No need to update - same IP!");
         } else {
-            echo "Set $dnsrecord->type to $dnsrecord->destination" . PHP_EOL;
+            _log( "Update $dnsrecord->type to $dnsrecord->destination");
             $dnsrecord->destination = $newIP;
             $toUpdateRecords[]      = $dnsrecord;
         }
+    } else {
+        _log("Ignoring $dnsrecord->hostname: not in list!");
     }
 }
-
-print_r($toUpdateRecords);
 
 if (!empty($toUpdateRecords)) {
 
@@ -122,20 +137,20 @@ if (!empty($toUpdateRecords)) {
 
 
     if (!$r || $r->status != 'success') {
-        echo "Get domains failed!";
-        print_r($r);
+        _log("Get domains failed!");
+        _log(print_r($r, true));
         exit;
     }
 
 } else {
-    echo "Nothing to update!";
+    _log( "Nothing to update!");
 }
 
 
 $r = $nc->logout(getenv('NC_KD'), getenv('NC_APIKEY'), $sessionId, 'NC DDNS Updater v' . $version);
 if (!$r || $r->status != 'success') {
-    echo "Logout failed!";
-    print_r($r);
+    _log("Logout failed!");
+    _log(print_r($r, true));
     exit;
 }
 
